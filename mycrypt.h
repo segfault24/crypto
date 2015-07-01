f6c47fa2ae53a6ac5832a749ee42c14915b1c362
Austin Selick-Bottos <segfault24@users.noreply.github.com>
2015-06-30 23:33:21 -0400

    algorithms are standalone programs now. any shared macros are in mycrypt.h

--- mycrypt.h
+++ mycrypt.h
@@ -1,16 +1,13 @@
 #ifndef MYCRYPT_H
 #define MYCRYPT_H
 
-#include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
 
 #define ROTL(a, s) (((a) << (s))|((a) >> (32-(s))))
 #define ROTR(a, s) (((a) >> (s))|((a) << (32-(s))))
-#define ENDIAN_SWAP(a) (((a>>24)&0x000000ff)|((a>>8)&0x0000ff00)|((a<<8)&0x00ff0000)|((a<<24)&0xff000000))
+#define ENDIANSWAP32(a) (((a>>24)&0x000000ff)|((a>>8)&0x0000ff00)|((a<<8)&0x00ff0000)|((a<<24)&0xff000000))
 
-uint32_t* md5_pad(const char * const data, const uint64_t len_bytes, uint64_t * const p_len_words);
-uint32_t* sha256_pad(const char * const data, const uint64_t len_bytes, uint64_t * const p_len_words);
+char* mymd5(const char *data, const uint64_t len);
+char* mysha256(const char * const data, const uint64_t len);
 
 #endif
-
