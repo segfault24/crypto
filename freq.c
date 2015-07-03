e2f2cb8e28c2243e4d8298bbbba3911a67be7654
Austin Selick-Bottos <segfault24@users.noreply.github.com>
2015-07-03 18:14:30 -0400

    improved to handle command line args

--- freq.c
+++ freq.c
@@ -1,138 +1,218 @@
 /* This program counts the frequency of each letter and bigram
  * in a file then outputs the results to stdout.
  */
- 
+
+/*char bigram[2][26] = {
+	{'t','h','i','e','a','r','n','a','o','n','h','e','s',
+	 'e','e','t','i','o','e','h','i','o','t','a','t','e'},
+	{'h','e','n','r','n','e','d','t','n','t','a','s','t',
+	 'n','d','o','t','u','a','i','s','r','i','s','e','t'}};*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-int main(int argc, char *argv[]) {
-	FILE *file;
-	int i, j, t_i, t_c, c_point, b_point;
-	char b, c, found;
-	int ccount[26][2]; // 0:char 1:count
-	int bcount[676][3]; // 0:char1 1:char2 2:count 
-	/*char bigram[2][26] = {
-		{'t','h','i','e','a','r','n','a','o','n','h','e','s',
-		 'e','e','t','i','o','e','h','i','o','t','a','t','e'},
-		{'h','e','n','r','n','e','d','t','n','t','a','s','t',
-		 'n','d','o','t','u','a','i','s','r','i','s','e','t'}};*/
+void print_usage()
+{
+	printf("usage: freq [-a|-b|-c] [-n] [-z] filename\n");
+	printf("   -a   print all counts\n");
+	printf("   -b   print only bigram counts\n");
+	printf("   -c   print only character counts (default)\n");
+	printf("   -n   don't sort before printing\n");
+	printf("   -z   print zero counts\n");
+	//TODO: printf("   -i   print natural frequencies info\n");
+}
 
-	if(argc < 2) {
-		printf("usage: %s filename\n", argv[0]);
-		return 1;
+int main(int argc, char* argv[])
+{
+	int file_arg, flag_sort, flag_single, flag_bigram, flag_zeroes;
+	
+	FILE *file;
+	int i, j, t_i, t_c;
+	char b, c;
+	int ccount[26][2]; // single character counts, 0:char 1:count
+	int bcount[26*26][3]; // bigram counts, 0:char1 1:char2 2:count 
+	
+	// default behaviors
+	file_arg = -1;
+	flag_sort = 1;
+	flag_single = 0;
+	flag_bigram = 0;
+	flag_zeroes = 0;
+	
+	// parse each command line argument
+	for(i=1; i<argc; i++)
+	{
+		// it's a flag
+		if(argv[i][0]=='-')
+		{
+			// go through all the flags in the argument
+			j = 1;
+			c = argv[i][j];
+			while(c != '\0')
+			{
+				// which flag is it
+				switch(c)
+				{
+					case 'a':
+						flag_single = 1;
+						flag_bigram = 1;
+						break;
+					case 'b':
+						flag_bigram = 1;
+						break;
+					case 'c':
+						flag_single = 1;
+						break;
+					case 'n':
+						flag_sort = 0;
+						break;
+					case 'z':
+						flag_zeroes = 1;
+						break;
+					default:
+						printf("Unknown option '%c'.\n", c);
+						print_usage();
+						return 1;
+				}
+				j++;
+				c = argv[i][j];
+			}
+		}
+		else
+		{
+			file_arg = i;
+		}
+	}
+	if(!flag_single && !flag_bigram)
+	{
+		flag_single = 1;
+	}
+	
+	// initialize the counter arrays
+	for(i=0; i<26; i++)
+	{
+		ccount[i][0] = 0x41+i;
+		ccount[i][1] = 0;
+		for(j=0; j<26; j++)
+		{
+			bcount[i*26+j][0] = 0x41+i;
+			bcount[i*26+j][1] = 0x41+j;
+			bcount[i*26+j][2] = 0;
+		}
 	}
 	
 	// open the input file
-	file = fopen(argv[1], "r");
-	if(file == 0) {
-		printf("Could not open file '%s' to read.\n", argv[1]);
+	if(file_arg == -1)
+	{
+		printf("No file specified.\n");
+		print_usage();
+		return 1;
+	}
+	file = fopen(argv[file_arg], "r");
+	if(file == 0)
+	{
+		printf("Could not open file '%s' to read.\n", argv[file_arg]);
 		return 1;
 	}
 	
-	// zero the counter arrays
-	memset(ccount, 0, sizeof(ccount[0][0])*26*2);
-	memset(bcount, 0, sizeof(bcount[0][0])*676*3);
-	c_point = 0;
-	b_point = 0;
-
+	// read the first two characters
+	do
+	{
+		b = toupper(fgetc(file));
+	} while(!isalpha(b) && b != EOF);
+	ccount[b-0x41][1]++; // process the first one special
+	do
+	{
+		c = toupper(fgetc(file));
+	} while(!isalpha(c) && c != EOF);
+	
 	// for each character and each pair of characters
-	b = toupper(fgetc(file));
-	c = toupper(fgetc(file));
-	while(b != EOF) {
-		// --- single character ---
-		// search the array
-		found = 0;
-		for(i=0; i<26; i++) {
-			// if we find an existing entry
-			if(b == ccount[i][0]) {
-				ccount[i][1]++;
-				found = 1;
-				break;
-			}
-		}
-		// add an entry
-		if(found == 0 && isalpha(b)) {
-			ccount[c_point][0] = b;
-			ccount[c_point][1] = 1;
-			c_point++;
-		}
-
-		// --- bigrams ---
-		// search the array
-		found = 0;
-		for(i=0; i<676; i++) {
-			// if we find an existing entry
-			if(b == bcount[i][0] && c == bcount[i][1]) {
-				bcount[i][2]++;
-				found = 1;
-				break;
-			}
-		}
-		// add an entry
-		if(found == 0 && isalpha(b) && isalpha(c)) {
-			bcount[b_point][0] = b;
-			bcount[b_point][1] = c;
-			bcount[b_point][2] = 1;
-			b_point++;
-		}
-
+	while(c != EOF)
+	{
+		ccount[c-0x41][1]++;
+		bcount[(b-0x41)*26+(c-0x41)][2]++;
+		
 		// move onto the next character, update pair
 		b = c;
-		c = toupper(fgetc(file));
+		do
+		{
+			c = toupper(fgetc(file));
+		} while(!isalpha(c) && c != EOF);
 	}
 	fclose(file);
-
-	// sort the single character array
-	for(i=0; i<26; i++) {
-		t_i = i;
-		t_c = ccount[i][1];
-		for(j=i+1; j<26; j++) {
-			if(ccount[j][1]>t_c) {
-				t_i = j;
-				t_c = ccount[j][1];
+	
+	if(flag_sort)
+	{
+		// sort the single character array
+		for(i=0; i<26; i++)
+		{
+			t_i = i;
+			t_c = ccount[i][1];
+			for(j=i+1; j<26; j++)
+			{
+				if(ccount[j][1]>t_c)
+				{
+					t_i = j;
+					t_c = ccount[j][1];
+				}
+			}
+			// swap em
+			b = ccount[t_i][0];
+			ccount[t_i][0] = ccount[i][0];
+			ccount[t_i][1] = ccount[i][1];
+			ccount[i][0] = b;
+			ccount[i][1] = t_c;
+		}
+		
+		// sort the bigram array
+		for(i=0; i<26*26; i++)
+		{
+			t_i = i;
+			t_c = bcount[i][2];
+			for(j=i+1; j<26*26; j++)
+			{
+				if(bcount[j][2]>t_c)
+				{
+					t_i = j;
+					t_c = bcount[j][2];
+				}
+			}
+			// swap em
+			b = bcount[t_i][0];
+			c = bcount[t_i][1];
+			bcount[t_i][0] = bcount[i][0];
+			bcount[t_i][1] = bcount[i][1];
+			bcount[t_i][2] = bcount[i][2];
+			bcount[i][0] = b;
+			bcount[i][1] = c;
+			bcount[i][2] = t_c;
+		}
+	}
+	
+	// print out counts
+	if(flag_single)
+	{
+		for(i=0; i<26; i++)
+		{
+			if(ccount[i][1]!=0 || flag_zeroes)
+			{
+				printf("%c:%u\n", ccount[i][0], ccount[i][1]);
 			}
 		}
-		// swap em
-		b = ccount[t_i][0];
-		ccount[t_i][0] = ccount[i][0];
-		ccount[t_i][1] = ccount[i][1];
-		ccount[i][0] = b;
-		ccount[i][1] = t_c;
 	}
-
-	// sort the bigram array
-	for(i=0; i<676; i++) {
-		t_i = i;
-		t_c = bcount[i][2];
-		for(j=i+1; j<676; j++) {
-			if(bcount[j][2]>t_c) {
-				t_i = j;
-				t_c = bcount[j][2];
+	if(flag_bigram)
+	{
+		for(i=0; i<26*26; i++)
+		{
+			if(bcount[i][2]!=0 || flag_zeroes)
+			{
+				printf("%c%c:%u\n", bcount[i][0], bcount[i][1], bcount[i][2]);
 			}
 		}
-		// swap em
-		b = bcount[t_i][0];
-		c = bcount[t_i][1];
-		bcount[t_i][0] = bcount[i][0];
-		bcount[t_i][1] = bcount[i][1];
-		bcount[t_i][2] = bcount[i][2];
-		bcount[i][0] = b;
-		bcount[i][1] = c;
-		bcount[i][2] = t_c;
 	}
-
-	// print out frequencies
-	if(argc == 3) {
-		printf("%c", (bcount[i][0]-0x41+22)%26+0x41);
-	} else {
-		for(i=0; i<26; i++) {
-			printf("'%c':\t%u\t\t'%c%c':\t%u\n",
-				ccount[i][0], ccount[i][1],
-				bcount[i][0], bcount[i][1], bcount[i][2]);
-		}
-	}
+	
 	return 0;
 }
 
